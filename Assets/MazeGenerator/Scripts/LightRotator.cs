f7b4e12fcd13464da8cda5784f9dfc7a80e33711
Kai Gustafson <kjgustafson1996@pointloma.edu>
2018-03-22 20:26:46 -0700

    lighting/skybox

--- Assets/MazeGenerator/Scripts/LightRotator.cs
+++ Assets/MazeGenerator/Scripts/LightRotator.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class LightRotator : MonoBehaviour {
 
     public GameObject player;
+    Material sky;
     public Transform center;
+    public Transform stars;
     public Vector3 axis = Vector3.right;
     public Vector3 desiredPosition;
     public float radius = .1f;
     public float radiusSpeed = 0.5f;
     public float rotationSpeed = 80.0f;
+    public float nightIntensity = .001f;
+    public float noonIntensity = 1f;
 
     void Start()
     {
+        sky = RenderSettings.skybox;
         player = GameObject.FindWithTag("Player");
         center = player.transform;
         transform.position = (transform.position - center.position).normalized * radius + center.position;
@@ -21,17 +26,18 @@ public class LightRotator : MonoBehaviour {
 
     void Update()
     {
+        stars.rotation = transform.rotation;
         transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
         desiredPosition = (transform.position - center.position).normalized * radius + center.position;
         transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
-
+        
         if (transform.position.y < 0)
         {
-            RenderSettings.ambientIntensity = .01f;
+            RenderSettings.ambientIntensity = nightIntensity;
         }
         else
         {
-            RenderSettings.ambientIntensity = 1f;
+            RenderSettings.ambientIntensity = noonIntensity;
         }
     }
 }
